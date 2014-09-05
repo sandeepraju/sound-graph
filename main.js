667248cbc8c3cb8eed8da9bbb705444ac91e9f0b
Sandeep Raju <me@sandeepraju.in>
2014-09-05 10:51:54 +0000

    adds event handler

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.bodyParser());
+
 app.get('/', function(req, res){
   res.sendfile('templates/index.html');
+});
+
+
+app.get('/event', function(req, res){
+    io.emit('event', req.body)
 });
 
 io.on('connection', function(socket){
