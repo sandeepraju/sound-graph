4de77ebd772edb28b133e39085057e9532e40a45
Varun <varun@plivo.com>
2014-09-05 14:48:57 +0530

    Run the app

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ from flask_sockets import Sockets
 app = Flask(__name__)
 sockets = Sockets(app)
 
+
 @sockets.route('/events')
 def echo_socket(ws):
     while True:
         message = ws.receive()
         ws.send(message)
 
+
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
