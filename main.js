9c4bad203c278faf0f1e3641d962bca65de93701
Varun <varun@plivo.com>
2014-09-05 16:54:03 +0530

    New line edits

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/static'));
-console.log(__dirname + '/static');
+app.use(bodyParser.json());
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-
 app.get('/', function(req, res){
   res.sendfile('templates/index.html');
 });
-
 
 app.get('/event', function(req, res){
     io.emit('event', req.body)
