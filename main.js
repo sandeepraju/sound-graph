a87420631743b85e549f370f2d1592b0352434cd
Sandeep Raju <me@sandeepraju.in>
2014-09-05 11:20:11 +0000

    body parser

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 
-app.use(express.bodyParser());
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.sendfile('templates/index.html');
@@ -11,6 +12,7 @@ app.get('/', function(req, res){
 
 app.get('/event', function(req, res){
     io.emit('event', req.body)
+    res.send({'status': 'success'});
 });
 
 io.on('connection', function(socket){
